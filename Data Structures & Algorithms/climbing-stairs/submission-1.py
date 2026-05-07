e8e611e44eca9b8975fe8034eb464924838dc3b0
Lina Oumalek <linaoumalek17@gmail.com>
2026-05-07 12:22:48 +0100

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        def dfs(n, memo):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 1
+            if n < 0:
+                return 0
+            
+            memo[n] = dfs(n-1, memo) + dfs(n-2, memo)
+            return memo[n]
+        return dfs(n, dict())
