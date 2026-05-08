5f352845ab0875e16b5f0cad34f93c1308ba9be4
Lina Oumalek <linaoumalek17@gmail.com>
2026-05-08 10:05:29 +0100

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def dfs(i, memo):
+            if i in memo:
+                return memo[i]
+            if i >= len(nums):
+                return 0
+            memo[i] = max(nums[i] + dfs(i+2, memo), dfs(i+1, memo))
+            return memo[i] 
+        memo = dict()
+        return max(dfs(0, memo), dfs(2, memo))
